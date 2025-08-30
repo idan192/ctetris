cacd6c17f8a16dac33e1ecf2f8914b13abc0c020
Idan Izhaki <idan192@users.noreply.github.com>
2025-08-30 16:09:15 -0700

    Revert to working SDL tetris implementation

--- src/game.h
+++ src/game.h
@@ -1,63 +1,136 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <stddef.h>
-#include <stdint.h>
+#define BLOCK_SIZE_PIXELS 32
+#define COORDS_SIZE 10
+#define TETROMINO_MAP_SIZE 8
 
-#define MINO_COORDS_SIZE 8
+#include <SDL3/SDL_render.h>
+
+typedef struct {
+  SDL_Texture *sheet;
+  int h;
+  int w;
+} SpriteSheet_t;
+
+typedef enum TranslationDirection {
+  TRANSLATION_DIRECTION_LEFT = -1,
+  TRANSLATION_DIRECTION_RIGHT = 1,
+} TranslationDirection;
 
 typedef enum {
-  TETROMINO_SHAPE_I,
-  TETROMINO_SHAPE_J,
-  TETROMINO_SHAPE_L,
-  TETROMINO_SHAPE_O,
-  TETROMINO_SHAPE_S,
-  TETROMINO_SHAPE_T,
-  TETROMINO_SHAPE_Z,
-} ETetrominoShape;
-typedef enum { TETROMINO_STATE_ACTIVE } ETetrominoState;
+  USER_INPUT_NONE,
+  USER_INPUT_ROTATE_LEFT,
+  USER_INPUT_ROTATE_RIGHT,
+  USER_INPUT_MOVE_LEFT,
+  USER_INPUT_MOVE_RIGHT,
+  USER_INPUT_SOFT_DROP,
+  USER_INPUT_HARD_DROP,
+  USER_INPUT_PAUSE,
+  USER_INPUT_SHOW_DEBUG,
+} UserInput_t;
+
+typedef enum {
+  TETROMINO_SHAPE_TAG_I,
+  TETROMINO_SHAPE_TAG_O,
+  TETROMINO_SHAPE_TAG_T,
+  TETROMINO_SHAPE_TAG_S,
+  TETROMINO_SHAPE_TAG_Z,
+  TETROMINO_SHAPE_TAG_J,
+  TETROMINO_SHAPE_TAG_L
+} TetrominoShapeTag;
+
+typedef enum {
+  TETROMINO_STATE_ACTIVE,
+  TETROMINO_STATE_LOCKED,
+  TETROMINO_STATE_EXPLODED,
+  TETROMINO_STATE_INTERSECTED,
+  TETROMINO_STATE_PENDING,
+} TetrominoState_t;
+
+// TODO: This should be called TetrominoShape imo, and it should include deg rotated.
+typedef struct {
+  int row0, col0;
+  size_t size;
+  TetrominoShapeTag t;
+  int const *map;
+} TetrominoMatrix;
+
+// TODO: deg_rot shoud be part of the Matrix
+// Matrix should be called shape
+// If the Tetromino "explodes" or otherwise "distorts"
+// we should expand the area of the shape matrix so that
+// those pieces can be rotated in an "exploded form"
+// And "wall kicks" for exploded minos could end up inside a single gap...
+typedef struct {
+  SpriteSheet_t *sheet;
+  SDL_FRect *clip;
+  int deg_rot;
+  TetrominoState_t state;
+  TetrominoMatrix *mat;
+  // Whether a block should be hidden
+  uint8_t hide_mask;
+} Tetromino_t;
 
 typedef struct {
-  size_t row0, col0;
-  uint32_t deg;
-  size_t const *mino_coords;
-  size_t mino_shift[MINO_COORDS_SIZE];
-  uint8_t mino_mask;
-  uint8_t bound_size;
-  ETetrominoShape shape;
-  ETetrominoState state;
-} Tetromino;
+  Tetromino_t **tetrominos;
+  size_t size;
+  size_t cnt;
+} TetrominoCollection_t;
 
 typedef struct {
-  Tetromino **arr;
-  size_t cap, cnt;
-} TetrominoCollection;
+  size_t cols;
+  size_t rows;
+  Tetromino_t ***arr;
+} GameBoard_t;
 
 typedef struct {
-  size_t rows, cols;
-  TetrominoCollection *coll;
-} TetrominoWell;
+  int display_w;
+  int display_h;
+  double tick_rate;
+  GameBoard_t *board;
+  GameBoard_t *previous_board;
+  UserInput_t input;
+  TetrominoCollection_t *tetrominos;
+} GameApp_t;
 
-typedef struct {
-  TetrominoWell *well;
-} GameState;
+Tetromino_t *Tetromino_init(TetrominoShapeTag const shape, int row, int col);
+void Tetromino_free(Tetromino_t *t);
+void Tetromino_rotate(Tetromino_t *const t, int const deg);
 
-Tetromino *Tetromino_init(ETetrominoShape const shape, size_t const row, size_t const col);
-void Tetromino_free(Tetromino *t);
-void Tetromino_hide_mino(Tetromino *const t, uint8_t const row);
-void Tetromino_shift_mino(Tetromino *const t, size_t const mino_idx, size_t const row_shift);
-void Tetromino_translate(Tetromino *const t, size_t const row_shift, size_t const col_shift);
+// TODO: maybe instead just use an Enum with PI, 2PI, 3PI
+void TetrominoMatrix_free(TetrominoMatrix *o);
+int *TetrominoMatrix_rotate(TetrominoMatrix const *const mat, int const dega);
 
-TetrominoCollection *TetrominoCollection_init(size_t const cap);
-void TetrominoCollection_free(TetrominoCollection *coll);
-void TetrominoCollection_push(TetrominoCollection *const coll, Tetromino *const t);
-void TetrominoCollection_resize(TetrominoCollection *const coll);
+TetrominoCollection_t *TetrominoCollection_init(size_t const size);
+void TetrominoCollection_free(TetrominoCollection_t *col);
+void TetrominoCollection_push(TetrominoCollection_t *col, Tetromino_t *const new);
+bool TetrominoCollection_contains_active(TetrominoCollection_t const *const col);
+void TetrominoCollection_render(TetrominoCollection_t *col, SDL_Renderer *render);
 
-size_t *TetrominoWell_coords(Tetromino const *const t);
-TetrominoWell *TetrominoWell_init(void);
-void TetrominoWell_free(TetrominoWell *t);
+// TODO: this shoul return a sprite sheet
+int SpriteSheet_init(SDL_Renderer *renderer, SpriteSheet_t *const sheet, char const *const sheet_path);
+void SpriteSheet_free(SpriteSheet_t *o);
 
-GameState *GameState_init(void);
-void GameState_free(GameState *t);
+GameBoard_t *GameBoard_init(int cols, int rows);
+void GameBoard_free(GameBoard_t *o);
+bool GameBoard_collision(GameBoard_t const *const board, Tetromino_t *const t, size_t const rows, size_t const cols);
+int *GameBoard_get_tetromino_coords(Tetromino_t const *const tetromino);
+void GameBoard_clear_full_rows(GameBoard_t *board);
+void GameBoard_copy(GameBoard_t *const dest, GameBoard_t const *const src);
+void GameBoard_print_debug(GameBoard_t const *const board);
+void GameBoard_rotate_pi(GameBoard_t *const board, Tetromino_t *const tetromino);
+void GameBoard_insert_tetromino(GameBoard_t *const board, Tetromino_t *const new_tetromino);
+void GameBoard_translate_down(GameBoard_t *const board, Tetromino_t *const tetromino);
+void GameBoard_translate_left(GameBoard_t *const board, Tetromino_t *const tetromino);
+void GameBoard_translate_right(GameBoard_t *const board, Tetromino_t *const tetromino);
+void GameBoard_update(GameBoard_t *board, GameApp_t *app_state);
+void GameBoard_destroy_mino(Tetromino_t *const tetromino, int const row, int const col);
+
+GameApp_t *GameApp_init(void);
+void GameApp_handle_input(GameApp_t *state);
+void GameApp_update(GameApp_t *state);
+
+void SpriteSheet_tetrominos(SpriteSheet_t *sheet);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,23 +1,27 @@
-#include "game.h"
+#include <SDL3/SDL_oldnames.h>
+#include <stdio.h>
 #define SDL_MAIN_USE_CALLBACKS 1
 
 #include "_gen/cmake_variables.h"
+#include "game.h"
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_filesystem.h>
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_log.h>
 #include <SDL3/SDL_main.h>
-#include <SDL3/SDL_render.h>
 #include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_scancode.h>
+#include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_surface.h>
+#include <SDL3/SDL_timer.h>
 #include <SDL3/SDL_video.h>
-#include <stdio.h>
-#include <stdlib.h>
 
+static GameApp_t *app = NULL;
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
-static Tetromino *active_tetromino = NULL;
-
-#define WINDOW_WIDTH 640
-#define WINDOW_HEIGHT 480
-#define BLOCK_SIZE_PIXELS 32
+static SpriteSheet_t tmp_sheet = {0};
 
 void stdoutLog(void *UNUSED(userdata), int UNUSED(category), SDL_LogPriority UNUSED(priority), const char *message) {
   printf("%s\n", message);
@@ -39,16 +43,17 @@ SDL_AppResult SDL_AppInit(void **UNUSED(appstate), int UNUSED(argc), char *UNUSE
     return SDL_APP_FAILURE;
   }
 
-  // TODO: initialize GameState
+  app = GameApp_init();
 
-  if (!SDL_CreateWindowAndRenderer(CMAKE_PROJECT_NAME, WINDOW_WIDTH, WINDOW_HEIGHT,
-                                   /* SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS, */
-                                   0, &window, &renderer)) {
+  if (!SDL_CreateWindowAndRenderer(CMAKE_PROJECT_NAME, app->board->cols * BLOCK_SIZE_PIXELS,
+                                   app->board->rows * BLOCK_SIZE_PIXELS,
+                                   /* SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS */ 0, &window, &renderer)) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Init window and renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
-  active_tetromino = Tetromino_init(TETROMINO_SHAPE_I, 1, WINDOW_WIDTH / (2 * BLOCK_SIZE_PIXELS));
+  SpriteSheet_init(renderer, &tmp_sheet, "tetrominos.bmp");
+  SpriteSheet_tetrominos(&tmp_sheet);
 
   return SDL_APP_CONTINUE;
 }
@@ -57,31 +62,54 @@ SDL_AppResult SDL_AppEvent(void *UNUSED(appstate), SDL_Event *event) {
   if (event->type == SDL_EVENT_QUIT) {
     return SDL_APP_SUCCESS;
   }
+
+  if (event->type == SDL_EVENT_KEY_DOWN) {
+    switch (event->key.scancode) {
+    case SDL_SCANCODE_UP:
+      app->input = USER_INPUT_ROTATE_RIGHT;
+      break;
+    case SDL_SCANCODE_K:
+      app->input = USER_INPUT_ROTATE_RIGHT;
+      break;
+    case SDL_SCANCODE_J:
+      app->input = USER_INPUT_ROTATE_LEFT;
+      break;
+    case SDL_SCANCODE_LEFT:
+      app->input = USER_INPUT_MOVE_LEFT;
+      break;
+    case SDL_SCANCODE_RIGHT:
+      app->input = USER_INPUT_MOVE_RIGHT;
+      break;
+    case SDL_SCANCODE_DOWN:
+      app->input = USER_INPUT_SOFT_DROP;
+      break;
+    case SDL_SCANCODE_SPACE:
+      app->input = USER_INPUT_HARD_DROP;
+      break;
+    case SDL_SCANCODE_D:
+      app->input = USER_INPUT_SHOW_DEBUG;
+      break;
+    default:
+      app->input = USER_INPUT_NONE;
+      break;
+    }
+  } else {
+    app->input = USER_INPUT_NONE;
+  }
+
   return SDL_APP_CONTINUE;
-  ;
 }
 
 SDL_AppResult SDL_AppIterate(void *UNUSED(appstate)) {
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+  GameApp_update(app);
+
+  SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0);
   SDL_RenderClear(renderer);
 
-  size_t *coords = TetrominoWell_coords(active_tetromino);
-  SDL_SetRenderDrawColor(renderer, 0, 255, 255, SDL_ALPHA_OPAQUE);
-  SDL_FRect rect = {.w = BLOCK_SIZE_PIXELS, .h = BLOCK_SIZE_PIXELS};
-  for (size_t i = 0; i < MINO_COORDS_SIZE; i += 2) {
-    rect.y = coords[i] * BLOCK_SIZE_PIXELS;
-    rect.x = coords[i + 1] * BLOCK_SIZE_PIXELS;
-    SDL_RenderFillRect(renderer, &rect);
-  }
-  free(coords);
+  TetrominoCollection_render(app->tetrominos, renderer);
 
   SDL_RenderPresent(renderer);
   return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *UNUSED(appstate), SDL_AppResult UNUSED(result)) {
-  Tetromino_free(active_tetromino);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-}
+void SDL_AppQuit(void *UNUSED(appstate), SDL_AppResult UNUSED(result)) {}
