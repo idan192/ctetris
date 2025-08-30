488348275d4f36b81625353192630400b6c31db5
Idan Izhaki <idan192@users.noreply.github.com>
2025-08-30 13:12:51 -0700

    Render a sample tetromino

--- src/main.c
+++ src/main.c
@@ -5,12 +5,14 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 #include <stdio.h>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static TetrominoCollection *coll = NULL;
 
 void stdoutLog(void *UNUSED(userdata), int UNUSED(category), SDL_LogPriority UNUSED(priority), const char *message) {
   printf("%s\n", message);
@@ -22,24 +24,28 @@ SDL_AppResult SDL_AppInit(void **UNUSED(appstate), int UNUSED(argc), char *UNUSE
 
   SDL_SetAppMetadata(CMAKE_PROJECT_NAME, CMAKE_PROJECT_VERSION, "com.rse8.tetris");
 
-  if (!SDL_Init(SDL_INIT_VIDEO)) {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Init video: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
-  if (!SDL_Init(SDL_INIT_AUDIO)) {
+  if (SDL_Init(SDL_INIT_AUDIO) < 0) {
     SDL_LogError(SDL_LOG_CATEGORY_AUDIO, "Init Audio: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
   // TODO: initialize GameState
 
-  if (!SDL_CreateWindowAndRenderer(CMAKE_PROJECT_NAME, 100, 100,
-                                   /* SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS, */
-                                   0, &window, &renderer)) {
+  if (SDL_CreateWindowAndRenderer(CMAKE_PROJECT_NAME, 100, 100,
+                                  /* SDL_WINDOW_FULLSCREEN | SDL_WINDOW_BORDERLESS, */
+                                  0, &window, &renderer) != 0) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Init window and renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
+
+  coll = TetrominoCollection_init(10);
+  Tetromino *t = Tetromino_init(TETROMINO_SHAPE_I, 1, 1);
+  TetrominoCollection_push(coll, t);
 
   return SDL_APP_CONTINUE;
 }
@@ -49,16 +55,24 @@ SDL_AppResult SDL_AppEvent(void *UNUSED(appstate), SDL_Event *event) {
     return SDL_APP_SUCCESS;
   }
   return SDL_APP_CONTINUE;
-  ;
 }
 
 SDL_AppResult SDL_AppIterate(void *UNUSED(appstate)) {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
 
-  Tetromino *t = Tetromino_init(TETROMINO_SHAPE_I, 0, 0);
-  TetrominoCollection *coll = TetrominoCollection_init(100);
-  TetrominoCollection_push(coll, t);
-  TetrominoCollection_free(coll);
+  if (coll && coll->cnt > 0) {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    size_t *coords = TetrominoWell_coords(coll->arr[0]);
+    for (size_t i = 0; i < MINO_COORDS_SIZE; i += 2) {
+      SDL_Rect rect = {(int)(coords[i + 1] * 20), (int)(coords[i] * 20), 20, 20};
+      SDL_RenderFillRect(renderer, &rect);
+    }
+    free(coords);
+  }
+
+  SDL_RenderPresent(renderer);
   return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *UNUSED(appstate), SDL_AppResult UNUSED(result)) { return; }
+void SDL_AppQuit(void *UNUSED(appstate), SDL_AppResult UNUSED(result)) { TetrominoCollection_free(coll); }
